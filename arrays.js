9215ec8be57ee8d09433ae994f21a5a5a5a7a3a8
cheerful-animator-9488 <r.clarkemitchell@gmail.com>
2019-04-12 04:04:36 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,5 +2,6 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray (arr,elem) {
 let newArr = [...arr].unshift(elem)
+console.log(newArr)
 return newArr
 }
