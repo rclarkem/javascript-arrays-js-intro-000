59b366b5c8e784564346a848724bb86c2c5f5b7a
cheerful-animator-9488 <r.clarkemitchell@gmail.com>
2019-04-12 04:16:51 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,3 +10,14 @@ function destructivelyAddElementToBeginningOfArray (array, element) {
   array.unshift(element)
   return array
 }
+
+function addElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+
+function destructivelyAddElementToEndOfArray (array,element){
+  let newArr = [...array]
+  newArr.push(element)
+  return newArr
+}
